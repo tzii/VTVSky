a9f87bc406a1894cc3f85081985b1182a5904cff
Kyo-Jounin <tuanvundvn@gmail.com>
2020-06-23 13:30:27 +0700

    Thêm form chỉnh sửa phiếu đặt chỗ và bán vé

---
+++ Presentation Layer/FormDigital/frmPhieuDatChoEditing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation_Layer.FormDigital
+{
+    public partial class frmPhieuDatChoEditing : Form
+    {
+        public frmPhieuDatChoEditing()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPhieuDatChoEditing_Load(object sender, EventArgs e)
+        {
+            guna2ShadowForm1.SetShadowForm(this);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
