23e7666182ffe59da8b30477b452454412beb455
Kyo-Jounin <tuanvundvn@gmail.com>
2020-06-24 08:26:37 +0700

    Thêm class User

---
+++ Data Transfer Objects/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Transfer_Objects
+{
+    public class User
+    {
+        public static int id;
+        public static string Username;
+        public static string Password;
+        public static string Per;
+        public static string Name;
+        public static DateTime NgaySinh;
+        public static string QueQuan;
+        public static string DienThoai;
+
+        public static string Permission
+        {
+            get
+            {
+                if (Per == "ADMIN") return "Admin";
+                if (Per == "MANAGER") return "Quản Lý";
+                if (Per == "STAFF") return "Nhân Viên";
+                return "######";
+            }
+        }
+    }
+}
