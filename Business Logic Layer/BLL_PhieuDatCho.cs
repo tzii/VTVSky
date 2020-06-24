0e90756063b9c0204f9793883bedef770777b1e4
Kyo-Jounin <tuanvundvn@gmail.com>
2020-06-24 13:30:39 +0700

    Hoàn thiện chức năng get, search trong Ve

---
+++ Business Logic Layer/BLL_PhieuDatCho.cs
@@ -0,0 +1,46 @@
+﻿using Data_Transfer_Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Logic_Layer
+{
+    public class BLL_PhieuDatCho
+    {
+        public static List<PhieuDatCho> GetPhieuDatChos()
+        {
+            return new List<PhieuDatCho>();
+        }
+        public static List<PhieuDatCho> SearchMaVe(string maVe)
+        {
+            return new List<PhieuDatCho>();
+        }
+        public static List<PhieuDatCho> SearchMaCB(string maCB)
+        {
+            return new List<PhieuDatCho>();
+        }
+        public static List<PhieuDatCho> SearchTenHK(string tenHK)
+        {
+            return new List<PhieuDatCho>();
+        }
+        public static List<PhieuDatCho> SearchCMND(string cmnd)
+        {
+            return new List<PhieuDatCho>();
+        }
+        public static List<PhieuDatCho> SearchSDT(string sdt)
+        {
+            return new List<PhieuDatCho>();
+        }
+        public static List<PhieuDatCho> SearchThoiGian(string thoiGian)
+        {
+            return new List<PhieuDatCho>();
+        }
+        public static List<PhieuDatCho> SearchTinhTrang(string tinhTrang)
+        {
+            return new List<PhieuDatCho>();
+        }
+    }
+}
